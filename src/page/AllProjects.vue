b399183f142e51456c07397f8b6d9a3572c6c2e9
KerenBermeo <bermeokeren@gmail.com>
2025-04-09 19:49:04 -0500

    img ok

--- src/page/AllProjects.vue
+++ src/page/AllProjects.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { Github, ExternalLink } from 'lucide-vue-next'
+import goImage from '@/assets/foto/go.png'
+import dateTimeImage from '@/assets/foto/DateTime.png'
+import shortyUrlImage from '@/assets/foto/shortyurl.png'
+import moviGoImage from '@/assets/foto/logo_MoviGo.png'
+import portalLabImage from '@/assets/foto/portalLab.png'
+import pokeapiImage from '@/assets/foto/pokeapi.png'
 
 interface Project {
   id: number;
@@ -18,7 +24,7 @@ const projects = ref<Project[]>([
     id: 1,
     title: "API FlashMentor",
     description: "Esta es la Api es la base de para una aplicacion web de estudio con la cual se puede crear una pagina web con tarjetas de memoria que faciliten el aprendizaje.",
-    image: "/src/assets/foto/fotoDev.png?height=200&width=200",
+    image: goImage,
     technologies: ["Go", "GORM", "Gin", "MySQL"],
     githubLink: "https://github.com/KerenBermeo/FlashMentor",
     liveLink: "https://proyecto1.com"
@@ -27,7 +33,7 @@ const projects = ref<Project[]>([
     id: 2,
     title: "DATETIME API",
     description: "Una API versátil que ofrece múltiples funcionalidades relacionadas con la manipulación y conversión de fechas y horas. ",
-    image: "/src/assets/foto/fotoDev.png?height=200&width=200",
+    image: dateTimeImage,
     technologies: ["Python", "FastAPI"],
     githubLink: "https://github.com/KerenBermeo/DateTime",
     liveLink: "https://proyecto2.com"
@@ -36,7 +42,7 @@ const projects = ref<Project[]>([
     id: 3,
     title: "ShortyURL API",
     description: "ShortyURL es una API de acortamiento de URLs desarrollada con Python y ApiFast que convierte URLs largas en enlaces cortos.",
-    image: "/src/assets/foto/fotoDev.png?height=200&width=200",
+    image: shortyUrlImage,
     technologies: ["Python", "Flask"],
     githubLink: "https://github.com/KerenBermeo/ShortyURL",
     liveLink: "https://proyecto3.com"
@@ -45,7 +51,7 @@ const projects = ref<Project[]>([
     id: 4,
     title: "MoviGo",
     description: "Aplicacion de escritorio desarrollada en la formacion tecnica con tematica de servicio de transporte",
-    image: "/src/assets/foto/fotoDev.png?height=200&width=200",
+    image: moviGoImage,
     technologies: ["Java", "MySQL"],
     githubLink: "https://github.com/KerenBermeo/MoviGo",
     liveLink: "https://proyecto4.com"
@@ -54,7 +60,7 @@ const projects = ref<Project[]>([
     id: 5,
     title: "Portal Lab",
     description: "Pagina web de un laboratorio que simula la facturacion de un laboratorio medico.",
-    image: "/src/assets/foto/fotoDev.png?height=200&width=200",
+    image: portalLabImage,
     technologies: ["PHP", "HTML", "CSS", "Javascript", "MariaDB"],
     githubLink: "https://github.com/KerenBermeo/PortalLab",
     liveLink: "https://proyecto5.com"
@@ -63,7 +69,7 @@ const projects = ref<Project[]>([
     id: 6,
     title: "cosumo_pokeapi",
     description: "Experimento de consumo de pokeAPI y utilizacion de EJS",
-    image: "/src/assets/foto/fotoDev.png?height=200&width=200",
+    image: pokeapiImage ,
     technologies: ["Jacascript", "EJS", "Node.js", "Express", "Axios"],
     githubLink: "https://github.com/KerenBermeo/cosumo_pokeapi",
     liveLink: "https://proyecto6.com"
